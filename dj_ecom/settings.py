30ef03755c372cbb268c85b4738f42f2ebd2f7f4
Noel Quirke <noelquirke900@hotmail.com>
2020-08-26 18:41:01 +0000

    Adding forms, admin and signals

--- dj_ecom/settings.py
+++ dj_ecom/settings.py
@@ -147,10 +147,13 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
